52a8dff9817abe70b7c90870572a06576f43f892
Dinne Kopelevich <dinne22@hotmail.com>
2023-08-28 13:19:28 -0600

    Implement serveless functions

---
+++ api/index.js
@@ -0,0 +1,48 @@
+const app = require("express")();
+const cors = require("cors");
+
+require("dotenv").config({ path: "./.env" });
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
+app.use(cors());
+
+// app.set("port", process.env.PORT || 5252);
+
+app.get("/api", (_, res) => {
+  res.send("Success");
+});
+
+app.get("/api/config", (_, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/api/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "EUR",
+      amount: 1999,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
+app.listen(5252, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
+
+module.exports = app;
